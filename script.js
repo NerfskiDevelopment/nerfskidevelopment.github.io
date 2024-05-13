00b612c3d8b0d8995be4bb52046301bda691d4cc
NerfskiDevelopment <64707438+NerfskiDevelopment@users.noreply.github.com>
2024-05-12 19:07:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 window.onload = function(){ //Setting game size after window has loaded, or else the unityContainer will be null.
 
-  var gameWidth =  window.screen.width - 50;
+  var gameWidth =  window.screen.width - 150;
 
-  var gameHeight = window.screen.height * .8; //Setting game height to 80% of the users screen height.  I don't show the full amount so that they don't think it's true full screen and they know a website is below.
+  var gameHeight = window.screen.height * .7 ; //Setting game height to 70% of the users screen height.  I don't show the full amount so that they don't think it's true full screen and they know a website is below.
 
   var gameFrame = document.getElementById("gameIFrame");
 
-  
-  //shrinking the window a little bit
-  gameFrame.width = gameWidth - 80;
-  gameFrame.height = gameHeight - 100;
 
   //dealing with scrollbar issues
   gameFrame.width = gameWidth + 25;
