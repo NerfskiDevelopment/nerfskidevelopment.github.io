e3e6e039aea2be46f676b711a885b3bb46613bf3
NerfskiDevelopment <64707438+NerfskiDevelopment@users.noreply.github.com>
2024-05-12 19:09:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ window.onload = function(){ //Setting game size after window has loaded, or else
 
   var gameWidth =  window.screen.width - 150;
 
-  var gameHeight = window.screen.height * .7 ; //Setting game height to 70% of the users screen height.  I don't show the full amount so that they don't think it's true full screen and they know a website is below.
+  var gameHeight = window.screen.height * .75 ; //Setting game height to 70% of the users screen height.  I don't show the full amount so that they don't think it's true full screen and they know a website is below.
 
   var gameFrame = document.getElementById("gameIFrame");
 
 
   //dealing with scrollbar issues
   gameFrame.width = gameWidth + 25;
-  gameFrame.height = gameHeight + 40; //The +40 seems to fix issue with their being a scroll bar for the game area.
+  gameFrame.height = gameHeight + 50; //The +50 seems to fix issue with their being a scroll bar for the game area.
   gameFrame.src += "?gameWidth=" + gameWidth + "&gameHeight=" + gameHeight;
 }
